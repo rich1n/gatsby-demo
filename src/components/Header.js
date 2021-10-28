07283daf8144ca3004e26915dd8056773c7abb72
Richard <ricard122@gmail.com>
2021-10-27 21:38:25 -0400

    update menu hover + font colors + logo width

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,25 +10,24 @@ const Header = () => {
         <StaticImage
           src="../images/logo.png"
           alt="Company Logo"
-          width={100}
-          height={100}
+          width={150}
           placeholder="tracedSVG"
           quality="80" />
       </Link>
       <div className="navMain">
-        <Link to="#" className="navLink">
+        <Link to="/" className="navLink">
           Home
         </Link>
-        <Link to="#" className="navLink">
+        <Link to="/" className="navLink">
           About Me
         </Link>
-        <Link to="#" className="navLink">
+        <Link to="/" className="navLink">
           Freebies
         </Link>
-        <Link to="#" className="navLink">
+        <Link to="/" className="navLink">
           Blog
         </Link>
-        <Link to="#" className="navLink">
+        <Link to="/" className="navLink">
           Contact
         </Link>
       </div>
